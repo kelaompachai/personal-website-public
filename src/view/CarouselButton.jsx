6456689c2c52f855e3f6081d85766fe656967f2a
kelaompachai <141376761+kelaompachai@users.noreply.github.com>
2025-05-26 16:49:04 -0400

    make cursor a pointer when it hovers on the next/previous recommendation buttons
    Signed-off-by: kelaompachai <141376761+kelaompachai@users.noreply.github.com>
    
    change snapshots to match new cursor pointer feature
    
    Signed-off-by: kelaompachai <141376761+kelaompachai@users.noreply.github.com>

--- src/view/CarouselButton.jsx
+++ src/view/CarouselButton.jsx
@@ -6,7 +6,7 @@ function CarouselButton(props) {
   const fill = direction === 'left' ? <Chevron direction="left" /> : <Chevron direction="right" />;
 
   return (
-    <button type="button" className="flex justify-center items-center size-24 border-2 border-well-read-500 rounded-3xl bg-well-read-200 hover:bg-well-read-300 active:transform active:translate-y-0.5" onClick={carousel}>{fill}</button>
+    <button type="button" className="flex justify-center items-center size-24 border-2 border-well-read-500 rounded-3xl bg-well-read-200 hover:bg-well-read-300 active:transform active:translate-y-0.5 cursor-pointer" onClick={carousel}>{fill}</button>
   );
 }
 
