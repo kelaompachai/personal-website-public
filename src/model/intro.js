839bd8ece3ee88975a3edca25f7e74500125d7f7
kelaompachai <141376761+kelaompachai@users.noreply.github.com>
2025-05-25 20:11:55 -0400

    change learning to fail test
    Signed-off-by: kelaompachai <141376761+kelaompachai@users.noreply.github.com>

--- src/model/intro.js
+++ src/model/intro.js
@@ -1,6 +1,6 @@
 const project = 'this website';
 const book = 'Frost\'s early poetry';
-const learning = 'gradle';
+const learning = 'chess';
 
 const text = `Hi, I'm Joe Paul, a software engineer who's passionate about what's true. I enjoy building websites (front and back end), solving leetcode problems, reading classics, and hiking. I'm currently working on ${project}, learning ${learning}, and reading ${book}. Feel free to email me if you'd like to talk about any of those things.`;
 
